1097345c35ab6189516147946805bead82a1c80e
Aaron-lv <674560606@qq.com>
2021-01-17 00:23:07 +0800

    更新

--- jd_scripts/docker_entrypoint.sh
+++ jd_scripts/docker_entrypoint.sh
@@ -9,12 +9,15 @@ fi
 echo "设定远程仓库地址..."
 cd /scripts
 git remote set-url origin $REPO_URL
-git reset --hard
 echo "git pull拉取最新代码..."
+git -C /scripts reset --hard
 git -C /scripts pull
 echo "npm install 安装最新依赖"
 npm install --prefix /scripts
 
+echo "更新jds仓库文件"
+git -C /jds reset --hard
+git -C /jds pull origin master
 echo "替换执行文件"
 cp /jds/jd_scripts/* /scripts/docker/
 echo "替换完成"
