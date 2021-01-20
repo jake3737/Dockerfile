8cfe540166921eefba5d486c76b8c4593548ffd4
jake3737 <50729332+jake3737@users.noreply.github.com>
2021-01-20 14:28:29 +0800

    Add files via upload

--- jd_scripts/shell_script_mod.sh
+++ jd_scripts/shell_script_mod.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 
 
-## 克隆shylocks仓库脚本
 if [ ! -d "/shylocks/" ]; then
     echo "未检查到shylocks仓库脚本，初始化下载相关脚本"
     git clone https://github.com/shylocks/Loon /shylocks
@@ -12,14 +11,45 @@ else
     cp -R /shylocks/jd*.js /scripts/
 fi
 
-## 红包雨
+## 直播间红包雨
 echo -e >> /scripts/docker/merged_list_file.sh
-echo "#更新红包雨" >> /scripts/docker/merged_list_file.sh
-echo -n "29,59 */1 * * * git -C /shylocks reset --hard && git -C /shylocks pull origin main && cp -R /shylocks/jd*.js /scripts/" >> /scripts/docker/merged_list_file.sh
-##加入shylocks仓库脚本cron
-sed -i "s/\n\n/\n/g" /shylocks/docker/crontab_list.sh
-sed -i "s/\n//g" /shylocks/docker/crontab_list.sh
-cat /shylocks/docker/crontab_list.sh >> /scripts/docker/merged_list_file.sh
+echo "#更新直播间红包雨" >> /scripts/docker/merged_list_file.sh
+echo "59 18-20/1 * * * git -C /shylocks reset --hard && git -C /shylocks pull origin main && cp -R /shylocks/jd_live_redrain2.js /scripts/" >> /scripts/docker/merged_list_file.sh
+echo "#直播间红包雨" >> /scripts/docker/merged_list_file.sh
+echo -n "0,1 19-21/1 * * * node /scripts/jd_live_redrain2.js |ts >> /scripts/logs/jd_live_redrain2.log 2>&1" >> /scripts/docker/merged_list_file.sh
+
+## 宝洁美发屋
+echo -e >> /scripts/docker/merged_list_file.sh
+echo "#宝洁美发屋" >> /scripts/docker/merged_list_file.sh
+echo -n "1 8,9 14-31/1 1 * node /scripts/jd_bj.js |ts >> /scripts/logs/jd_bj.log 2>&1" >> /scripts/docker/merged_list_file.sh
+## 工业品爱消除
+echo -e >> /scripts/docker/merged_list_file.sh
+echo "#工业品爱消除" >> /scripts/docker/merged_list_file.sh
+echo -n "20 * * * * node /scripts/jd_gyec.js |ts >> /scripts/logs/jd_gyec.log 2>&1" >> /scripts/docker/merged_list_file.sh
+## 盲盒抽京豆
+echo -e >> /scripts/docker/merged_list_file.sh
+echo "#盲盒抽京豆" >> /scripts/docker/merged_list_file.sh
+echo -n "1 7 * * * node /scripts/jd_mh.js |ts >> /scripts/logs/jd_mh.log 2>&1" >> /scripts/docker/merged_list_file.sh
+## 京东秒秒币
+echo -e >> /scripts/docker/merged_list_file.sh
+echo "#京东秒秒币" >> /scripts/docker/merged_list_file.sh
+echo -n "10 7 * * * node /scripts/jd_ms.js |ts >> /scripts/logs/jd_ms.log 2>&1" >> /scripts/docker/merged_list_file.sh
+## 神券京豆
+echo -e >> /scripts/docker/merged_list_file.sh
+echo "#神券京豆" >> /scripts/docker/merged_list_file.sh
+echo -n "1 7 13 1 * node /scripts/jd_super_coupon.js |ts >> /scripts/logs/jd_super_coupon.log 2>&1" >> /scripts/docker/merged_list_file.sh
+## 京东粉丝专享
+echo -e >> /scripts/docker/merged_list_file.sh
+echo "#京东粉丝专享" >> /scripts/docker/merged_list_file.sh
+echo -n "10 0 * * * node /scripts/jd_wechat_sign.js |ts >> /scripts/logs/jd_wechat_sign.log 2>&1" >> /scripts/docker/merged_list_file.sh
+## 小鸽有礼
+echo -e >> /scripts/docker/merged_list_file.sh
+echo "#小鸽有礼" >> /scripts/docker/merged_list_file.sh
+echo -n "5 7 * * * node /scripts/jd_xg.js |ts >> /scripts/logs/jd_xg.log 2>&1" >> /scripts/docker/merged_list_file.sh
+## 东东爱消除
+echo -e >> /scripts/docker/merged_list_file.sh
+echo "#东东爱消除" >> /scripts/docker/merged_list_file.sh
+echo -n "0 * * * * node /scripts/jd_xxl.js |ts >> /scripts/logs/jd_xxl.log 2>&1" >> /scripts/docker/merged_list_file.sh
 
 ## 京喜财富岛
 wget -O /scripts/jx_cfd.js https://raw.githubusercontent.com/moposmall/Script/main/Me/jx_cfd.js
@@ -30,7 +60,7 @@ echo -n "0 * * * * node /scripts/jx_cfd.js |ts >> /scripts/logs/jx_cfd.log 2>&1"
 wget -O /scripts/jx_cfdtx.js https://raw.githubusercontent.com/Aaron-lv/JavaScript/master/Task/jx_cfdtx.js
 echo -e >> /scripts/docker/merged_list_file.sh
 echo "#京喜财富岛提现" >> /scripts/docker/merged_list_file.sh
-echo "59 23 * * * sleep 58 && node /scripts/jx_cfdtx.js |ts >> /scripts/logs/jx_cfdtx.log 2>&1" >> /scripts/docker/merged_list_file.sh
+echo "59 23 * * * sleep 50 && node /scripts/jx_cfdtx.js |ts >> /scripts/logs/jx_cfdtx.log 2>&1" >> /scripts/docker/merged_list_file.sh
 
 
 
@@ -38,5 +68,3 @@ echo "59 23 * * * sleep 58 && node /scripts/jx_cfdtx.js |ts >> /scripts/logs/jx_
 sed -i "s/840266@2583822@2585219@2586018@1556311@2583822@2585256@2586023@2728968/754344@2695073@654824@1398507@2274010@715293@2751795@2796229@2484958/g" /scripts/jd_gyec.js
 ## 替换东东爱消除助力码
 sed -i "s/840266@2585219@2586018@1556311@2583822@2585256/754344@2695073@654824@1398507@2274010@715293@2751795@2796229@2484958/g" /scripts/jd_xxl.js
-## 替换个护爱消除助力码
-sed -i "s/840266@2585219@2586018@1556311@2583822@2585256/754344@2695073@654824@1398507@2274010@715293@2751795@2796229@2484958/g" /scripts/jd_xxl_gh.js
