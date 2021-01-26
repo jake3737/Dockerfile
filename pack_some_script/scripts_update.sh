e78590d4831aa6974ec41e61c6884abf46ca206c
jake3737 <50729332+jake3737@users.noreply.github.com>
2021-01-26 10:33:51 +0800

    Update scripts_update.sh

--- pack_some_script/scripts_update.sh
+++ pack_some_script/scripts_update.sh
@@ -61,8 +61,7 @@ function initBaidu() {
     echo package.json >>/baidu_speed/.git/info/sparse-checkout
     echo baidu_speed.js >>/baidu_speed/.git/info/sparse-checkout
     echo sendNotify.js >>/baidu_speed/.git/info/sparse-checkout
-    git pull origin master
-    cd Task
+    git pull origin master  
     npm install
 }
 
@@ -230,15 +229,15 @@ else
         git -C /baidu_speed reset --hard
         git -C /baidu_speed pull origin master
     fi
-    cp -r /baidu_speed/Task/baidu_speed.js /baidu_speed/Task/baidu_speed_use.js
-    sed -i "s/StartBody/BDCookie/g" /baidu_speed/Task/baidu_speed_use.js
-    sed -i "s/.*process.env.BAIDU_COOKIE.indexOf('\\\n')/else&/g" /baidu_speed/Task/baidu_speed_use.js
+    cp -r /baidu_speed/baidu_speed.js /baidu_speed/baidu_speed_use.js
+    sed -i "s/StartBody/BDCookie/g" /baidu_speed/baidu_speed_use.js
+    sed -i "s/.*process.env.BAIDU_COOKIE.indexOf('\\\n')/else&/g" /baidu_speed/baidu_speed_use.js
 
     if [ 0"$BAIDU_CRON" = "0" ]; then
         BAIDU_CRON="10 7-22 * * *"
     fi
     echo -e >>$defaultListFile
-    echo "$BAIDU_CRON sleep \$((RANDOM % 150)); node /baidu_speed/Task/baidu_speed_use.js >> /logs/baidu_speed.log 2>&1" >>$defaultListFile
+    echo "$BAIDU_CRON sleep \$((RANDOM % 150)); node /baidu_speed/baidu_speed_use.js >> /logs/baidu_speed.log 2>&1" >>$defaultListFile
 fi
 
 ##判断快手极速版COOKIE配置之后才会更新相关任务脚本
